5193a0e3f0eba3522bd9b66788e5ad210be43abc
samar <samarsekhar25@gmail.com>
2022-11-04 12:16:10 +0530

    React Props Class Method

--- React-BackUp/Props/02_PropsCls/App.js
+++ React-BackUp/Props/02_PropsCls/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Product from './PropsCls/Product';
 
 function App() {
   return (
     <div>
       <h1>Details</h1>
+      <Product/>
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div>
       <h1>Details</h1>
-      <Product/>
     </div>
   );
 }
