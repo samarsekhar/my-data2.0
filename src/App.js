9ac59f69b05a3e7fce4ef9f90e63606843a6ef61
samar <samarsekhar25@gmail.com>
2022-11-28 15:55:33 +0530

    Demo fetch API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React from 'react' 
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CrudTourists from './Components/CrudTourists';
 
 function App() {
   return (
-    <div className="container-fluid">
-      <main>
-        <CrudTourists/>
-      </main>
+    <div>
     </div>
   )
 }
